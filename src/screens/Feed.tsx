e8b5d00a5004b4f8946b9f6ce6aece970d8d5349
leeedev <leeesangheee@gmail.com>
2021-06-19 22:33:17 +0900

    seeFeed Query

--- src/screens/Feed.tsx
+++ src/screens/Feed.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 import styled from "styled-components/native";
+import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../../fragments";
 
 const View = styled.View`
   flex: 1;
@@ -11,7 +13,30 @@ const TouchableOpacity = styled.TouchableOpacity`
   background-color: yellow;
 `;
 
+const SEE_FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      ...PhotoFragment
+      user {
+        username
+        avatar
+      }
+      caption
+      comments {
+        ...CommentFragment
+      }
+      createdAt
+      isMine
+    }
+  }
+  ${PHOTO_FRAGMENT}
+  ${COMMENT_FRAGMENT}
+`;
+
 export default function Feed({ navigation }: any) {
+  const { data } = useQuery(SEE_FEED_QUERY);
+  console.log(data);
+
   return (
     <View>
       <Text>Feed</Text>
