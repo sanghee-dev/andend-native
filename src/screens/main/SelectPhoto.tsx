4c3805aa8bba6815b618aa48e88193507fc903e5
leeedev <leeesangheee@gmail.com>
2021-06-29 00:20:58 +0900

    Show photos with FlatList and make them selectable

--- src/screens/main/SelectPhoto.tsx
+++ src/screens/main/SelectPhoto.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import * as MediaLibrary from "expo-media-library";
+import { FlatList, Image, useWindowDimensions } from "react-native";
 import styled from "styled-components/native";
 import { colors } from "../../styles/colors";
 import { fonts } from "../../styles/fonts";
+import { Ionicons } from "@expo/vector-icons";
 
 const Container = styled.View`
   flex: 1;
@@ -18,40 +20,100 @@ const Bottom = styled.View`
   background-color: ${colors.grayLight};
 `;
 const RequestText = styled.Text`
-color:${colors.grayDark}
+  color: ${colors.grayDark};
   font-size: ${fonts.title};
 `;
+const ImageContainer = styled.TouchableOpacity``;
+const IconContainer = styled.View`
+  position: absolute;
+  right: 0;
+`;
+const IconCircle = styled.View``;
 
 export default function SelectPhoto() {
   const [isOk, setIsOk] = useState<boolean>(false);
   const [photos, setPhotos] = useState([]);
+  const [chosenPhotoUri, setChosenPhotoUri] = useState<string>("");
+
   const getPermissions = async () => {
     const { accessPrivileges, canAskAgain } =
       await MediaLibrary.getPermissionsAsync();
     if (accessPrivileges === "none" && canAskAgain) {
       const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
-      if (accessPrivileges !== "none") setIsOk(true);
+      if (accessPrivileges !== "none") {
+        setIsOk(true);
+        getPhotos();
+      }
     }
-    if (accessPrivileges !== "none") setIsOk(true);
+    if (accessPrivileges !== "none") {
+      setIsOk(true);
+      getPhotos();
+    }
   };
   const getPhotos = async () => {
-    if (isOk) {
-      const { assets: photos } = await MediaLibrary.getAssetsAsync();
-      setPhotos(photos);
-    }
+    const { assets: photos } = await MediaLibrary.getAssetsAsync();
+    setPhotos(photos);
+    setChosenPhotoUri(photos[0]?.uri);
   };
+  const choosePhoto = (uri): string => setChosenPhotoUri(uri);
 
   useEffect(() => {
     getPermissions();
-    getPhotos();
   }, []);
+
+  const numColumns = 3;
+  const borderWidth = 3;
+  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+
+  const renderItem = ({ item: photo }) => (
+    <ImageContainer
+      onPress={() => choosePhoto(photo.uri)}
+      style={{
+        borderColor: photo.uri === chosenPhotoUri ? colors.main : colors.white,
+        borderWidth: borderWidth,
+      }}
+    >
+      <Image
+        source={{ uri: photo.uri }}
+        style={{
+          width: windowWidth / numColumns - borderWidth * (numColumns - 1),
+          height: windowWidth / numColumns - borderWidth * (numColumns - 1),
+        }}
+      />
+      <IconContainer>
+        <Ionicons
+          name="checkmark-circle"
+          size={32}
+          color={photo.uri === chosenPhotoUri ? colors.main : colors.gray}
+          style={{
+            opacity: 0.6,
+          }}
+        />
+      </IconContainer>
+    </ImageContainer>
+  );
 
   return (
     <Container>
       <Top>
-        {isOk ? <></> : <RequestText>Plz grant permissions :(</RequestText>}
+        {!isOk && <RequestText>Plz grant permissions :(</RequestText>}
+        {isOk && chosenPhotoUri !== "" ? (
+          <Image
+            source={{ uri: chosenPhotoUri }}
+            style={{ width: (windowWidth / numColumns) * 2, height: "100%" }}
+          />
+        ) : (
+          <RequestText>Choose photo</RequestText>
+        )}
       </Top>
-      <Bottom></Bottom>
+      <Bottom>
+        <FlatList
+          data={photos}
+          renderItem={renderItem}
+          keyExtractor={(item: any) => "" + item.id}
+          numColumns={numColumns}
+        />
+      </Bottom>
     </Container>
   );
 }
