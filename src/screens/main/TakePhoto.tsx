effba74fbc004bbe6f6c6530048b3beaf08bdf66
leeedev <leeesangheee@gmail.com>
2021-06-29 15:03:05 +0900

    Add camera type control button

--- src/screens/main/TakePhoto.tsx
+++ src/screens/main/TakePhoto.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { useWindowDimensions } from "react-native";
 import { Camera } from "expo-camera";
 import styled from "styled-components/native";
 import { colors } from "../../styles/colors";
 import { fonts } from "../../styles/fonts";
 import SolidBtn from "../../components/buttons/TextBtn";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 const Container = styled.View`
   flex: 1;
+  background-color: ${colors.black};
 `;
 const RequestContainer = styled.View`
   flex: 1;
@@ -18,9 +19,39 @@ const RequestText = styled.Text`
   color: ${colors.grayDark};
   font-size: ${fonts.title};
 `;
+const Actions = styled.View`
+  flex: 0.3;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+const TakePhotoBtn = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 70px;
+  border-radius: 70px;
+  background-color: ${colors.white};
+  opacity: 0.8;
+`;
+const TakePhotoCircle = styled.View``;
+const TakePhotoBtnLine = styled.View`
+  width: 58px;
+  height: 58px;
+  border-radius: 58px;
+  border: 2px solid ${colors.black};
+  position: absolute;
+`;
+const TouchableOpacity = styled.TouchableOpacity`
+  opacity: 0.5;
+`;
 
 export default function TakePhoto() {
   const [isOk, setIsOk] = useState<boolean>(false);
+  const [cameraType, setCameraType] = useState<string>(
+    Camera.Constants.Type.front
+  );
+
   const getPermissions = async () => {
     const { canAskAgain, granted } = await Camera.getPermissionsAsync();
     if (granted) setIsOk(true);
@@ -30,30 +61,51 @@ export default function TakePhoto() {
     }
   };
   const requestPermissions = async () => await Camera.requestPermissionsAsync();
+  const onCameraType = () => {
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
+    } else setCameraType(Camera.Constants.Type.front);
+  };
 
   useEffect(() => {
     getPermissions();
   }, []);
 
-  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
-
   return (
     <Container>
-      {!isOk && (
+      {!isOk ? (
         <RequestContainer>
           <RequestText>Plz grant permissions :(</RequestText>
           <SolidBtn onPress={() => requestPermissions()} text="Access camera" />
         </RequestContainer>
+      ) : (
+        <>
+          <Camera
+            type={cameraType}
+            flashMode="auto"
+            autoFocus="on"
+            zoom={0}
+            whiteBalance="auto"
+            ratio="4:3"
+            style={{
+              flex: 1,
+            }}
+          />
+          <Actions>
+            <TakePhotoBtn>
+              <TakePhotoCircle></TakePhotoCircle>
+              <TakePhotoBtnLine></TakePhotoBtnLine>
+            </TakePhotoBtn>
+            <TouchableOpacity onPress={onCameraType}>
+              <Ionicons
+                name="camera-reverse-outline"
+                size={40}
+                style={{ color: colors.white }}
+              />
+            </TouchableOpacity>
+          </Actions>
+        </>
       )}
-      <Camera
-        type="front"
-        flashMode="auto"
-        autoFocus="on"
-        zoom={0}
-        whiteBalance="auto"
-        ratio="4:3"
-        style={{ width: windowWidth, height: windowHeight }}
-      />
     </Container>
   );
 }
